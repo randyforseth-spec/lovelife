4b2d8f549f8c4c3368de52bfe117041db89169ca
randyforseth-spec <randyforseth@gmail.com>
2025-10-30 12:18:42 -0600

    SHA of Potential Panel Fix pasted pixel-manifest.js

--- pixel-manifest.js
+++ pixel-manifest.js
@@ -1,2 +1,2 @@
 // commit-pinned URL to your working script:
-window.PXL_URL = "https://cdn.jsdelivr.net/gh/randyforseth-spec/lovelife@6a6655e0d4fabbba6eab55499d84c7fcee8fd66b/pixel-script.js";
+window.PXL_URL = "https://cdn.jsdelivr.net/gh/randyforseth-spec/lovelife@2c0ad4e9523929e95ad3eac55c8a370f19c0100d/pixel-script.js";
